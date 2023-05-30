45c106a2773d8c7556b5d00629e08d8463d1f44f
Brandon Orellana <brandon.orellana@hotmail.com>
2023-05-29 22:14:45 -0400

    Add docstring to test_views.py

--- tests/portugal_trip/test_views.py
+++ tests/portugal_trip/test_views.py
@@ -1,6 +1,12 @@
+"""
+This is my test for the views.
+"""
 # test_views.py
 
 def test_index_ok(client):
+    """
+    To ensure response status code is ok (200).
+    """
     response = client.get('/')
     # Assert that the status_code is 200 (OK)
     assert response.status_code == 200
