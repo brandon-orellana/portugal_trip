3cefacd8619661117f5ad6e4f9177071d0295546
Brandon Orellana <brandon.orellana@hotmail.com>
2023-07-19 19:21:28 -0400

    Updated views.py, post_detail.html, and topic_detail.html

--- blog/views.py
+++ blog/views.py
@@ -31,6 +31,10 @@ class HomeView(TemplateView):
     template_name = 'blog/home.html'
 
     def get_context_data(self, **kwargs):
+        """
+        Returns the context data for the HomeView.
+        Adds the most recent published posts to the context.
+        """
         context = super().get_context_data(**kwargs)
         latest_posts = models.Post.objects.published()\
                       .order_by('-published')[:10]
@@ -44,14 +48,26 @@ class AboutView(TemplateView):
     template_name = 'blog/about.html'
 
 class TopicDetailView(DetailView):
+    """
+    A view that displays a single topic and its related posts.
+    """
     model = models.Topic
     def get_queryset(self):
+        """
+        Returns the queryset for the TopicDetailView.
+        Filters the queryset to only include the topic with the given slug.
+        """
         queryset = super().get_queryset()
         return queryset.filter(slug=self.kwargs['slug'])
 
     def get_context_data(self, **kwargs):
+        """
+        Returns the context data for the TopicDetailView.
+        Adds the topic's related posts to the context.
+        """
         context = super().get_context_data(**kwargs)
-        topic_posts = models.Post.objects.published()\
+        topic = self.get_object()
+        topic_posts = models.Post.objects.published().filter(topics=topic)\
                       .order_by('-published')
         context.update({'topic_posts': topic_posts})
         return context
